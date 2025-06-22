6f3979f2dda82b7145e9dcec7869033cb0ccd299
Nisarg Patel <patelnisarg.3322@gmail.com>
2025-06-22 12:54:39 -0400

    fix: removed unused imports

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-import { AppRoutes } from './AppRoutes';
-import { Link } from 'react-router-dom';
-import { Button, Image, Container, Nav, NavDropdown, Navbar, Row, Col } from "react-bootstrap";
+import "./App.css";
+import { AppRoutes } from "./AppRoutes";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return (
-    
-       <AppRoutes />
-  );
+  return <AppRoutes />;
 }
 
 export default App;
